509fa4ad8507779930205f4ce703dcc493caa32a
Akshay <aktwo@princeton.edu>
2014-04-01 15:45:10 -0400

    Made website display most recent freefood email

--- getmail.py
+++ getmail.py
@@ -2,32 +2,25 @@ import imaplib
 import email
 import time
 import re
+import os
+from flask import Flask
 
-def getUID(mail):
+app = Flask(__name__)
+
+#print email_message.items() # print all headers
+
+@app.route('/')
+def getmail():
+  mail = imaplib.IMAP4_SSL('imap.gmail.com')
+  mail.login('freefood.princeton@gmail.com', 'freefood333')
+
   mail.select("FreeFood") # connect to FreeFood folder
-
   result, data = mail.uid('search', None, "ALL")
   uids = data[0] # data is a list.
   uid_list = uids.split() # ids is a space separated string
-  return uid_list[-1] # get the latest email's id
- 
+  uid = uid_list[-1] # get the latest email's id
 
-
-
-mail = imaplib.IMAP4_SSL('imap.gmail.com')
-mail.login('freefood.princeton@gmail.com', 'freefood333')
-
-saved_uid = -1
-while True:
-  # check every 5 seconds
-  latest_uid = getUID(mail)
-  if latest_uid == saved_uid:
-    print latest_uid
-    time.sleep(5)
-    continue
-  saved_uid = latest_uid
-
-  result, data = mail.uid('fetch', latest_uid, "(RFC822)")
+  result, data = mail.uid('fetch', uid, "(RFC822)")
   raw_email = data[0][1] # here's the body, which is raw text of the whole email
   # including headers and alternate payloads
 
@@ -52,10 +45,5 @@ while True:
   body_re = re.compile('\s*-----\s+You are receiving')
   split = body_re.split(body_raw)
   body = split[0]
-
-  print time.asctime(date)
-  print subject
-  print body
-
-  #print email_message.items() # print all headers
+  return time.asctime(date) + '</br>' + subject + '</br>' + body
 
