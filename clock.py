fec70209d0d68a3d996878d8d9c28277d033933c
Akshay <aktwo@princeton.edu>
2014-04-23 23:03:23 -0400

    Added post request to RESTful API on rails server

--- clock.py
+++ clock.py
@@ -1,5 +1,7 @@
 from apscheduler.scheduler import Scheduler
 import logging
+import requests
+import json
 import time
 import sys
 from gmail_checker import get_mail
@@ -14,17 +16,14 @@ current_email = None
 def timed_job():
   global current_email
   global buildings
-  print '-'*50
-  print 'Checking email now!'
   new_email = get_mail()
   if (current_email == None or current_email['uid'] != new_email['uid']):
     current_email = new_email
     print 'New email!'
-    print '\n'.join(new_email)
-    print 'The parsed data is'
     location_info = process_input(current_email['subject'] + ' ' + current_email['body'], buildings)
-    params = dict(current_email.items() + location_info.items())
-    print params
+    params = {'listing': dict(current_email.items() + location_info.items())}
+    headers = {'content-type': 'application/json'}
+    requests.post('http://hungrytigers.herokuapp.com/new_listing', data = json.dumps(params), headers = headers)
     sys.stdout.flush()
   else:
     print 'Sorry, nothing new here'
